15bdc7358601df873afe52106dd1010070dbef36
Matthieu Flce <flacelierematthieu@gmail.com>
2023-06-26 10:50:38 +0200

    feat: add sign base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './pages/sign/sign-up/sign-up.component';
+import { SignInComponent } from './pages/sign/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDqGiGF10IR9KMIn3qzlaOozUr_lds3RSw",
+        authDomain: "angular-projet-44368.firebaseapp.com",
+        databaseURL: "https://angular-projet-44368-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "angular-projet-44368",
+        storageBucket: "angular-projet-44368.appspot.com",
+        messagingSenderId: "840467376168",
+        appId: "1:840467376168:web:54fa746e4652751ef66d1d"
+    },
+};
